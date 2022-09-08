5b210d75aad42211b0b08c07d56cbe4ab9c36e90
blackcatunderrain <a27v@outlook.com>
2022-09-08 13:13:14 +0300

    Initial commit

---
+++ project/models.py
@@ -0,0 +1,44 @@
+from enum import unique
+
+from sqlalchemy import Column, String, Float, ForeignKey, Integer
+from sqlalchemy.orm import relationship
+
+from project.setup.db import models
+
+
+class Genre(models.Base):
+    __tablename__ = 'genres'
+
+    name = Column(String(100), unique=True, nullable=False)
+
+
+class Director(models.Base):
+    __tablename__ = 'directors'
+
+    name = Column(String(100), unique=True, nullable=False)
+
+
+class Movie(models.Base):
+    __tablename__ = 'movies'
+
+    title = Column(String(100))
+    description = Column(String(100))
+    trailer = Column(String(100))
+    year = Column(String(100), nullable=False)
+    rating = Column(Float, nullable=False)
+    genre_id = Column(Integer, ForeignKey(f"{Genre.__tablename__}.id"), nullable=False)
+    director_id = Column(Integer, ForeignKey(f"{Director.__tablename__}.id"), nullable=False)
+    genre = relationship("Genre")
+    director = relationship("Director")
+
+
+class User(models.Base):
+    __tablename__ = 'users'
+
+    email = Column(String(100), unique=True, nullable=False)
+    password = Column(String(380), nullable=False)
+    name = Column(String(100), unique=True, nullable=False)
+    surname = Column(String(100))
+    favorite_genre = Column(Integer, ForeignKey(f"{Genre.__tablename__}.id"), nullable=False)
+
+
