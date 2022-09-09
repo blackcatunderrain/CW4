a1c46af067a8eebd6faf622c8d1bc1d709eb3ae5
blackcatunderrain <a27v@outlook.com>
2022-09-09 18:00:20 +0300

    add part of auth

--- project/views/auth/auth.py
+++ project/views/auth/auth.py
@@ -1,3 +1,48 @@
-from flask_restx import Namespace
+from flask import request
+from flask_restx import Namespace, Resource
+from project.container import user_service
+from project.setup.api.models import user
+
 
 api = Namespace('auth')
+
+
+@api.route('/register/')
+class SignupView(Resource):
+    @api.marshal_with(user, as_list=True, code=200, description='OK')
+    def post(self):
+        """
+        Create a new user.
+        """
+        data = request.json
+        if data.get('email') and data.get('password'):
+            return user_service.create_user(email=data.get('email'), password=data.get('password')), 201
+        else:
+            return "Something went wrong.", 401
+
+
+@api.route('/login/')
+class SigninView(Resource):
+    @api.marshal_with(user, code=200, description='OK')
+    def post(self):
+        """
+        Sign in user.
+        """
+        data = request.json
+        if data.get('email') and data.get('password'):
+            return user_service.check(email=data.get('email'), password=data.get('password')), 200
+        else:
+            return "Something went wrong.", 401
+
+    @api.marshal_with(user, code=200, description='OK')
+    def put(self):
+        """
+        Update user token.
+        """
+        data = request.json
+        if data.get('access_token') and data.get('refresh_token'):
+            return user_service.update_toke(
+                access_token=data.get('access_token'), refresh_token=data.get('refresh_token')
+            ), 200
+        else:
+            return "Something went wrong.", 401
