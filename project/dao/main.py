66681a53505a63f43975009bb60913c609b7cfad
blackcatunderrain <a27v@outlook.com>
2022-09-09 15:16:23 +0300

    add models services and create db

--- project/dao/main.py
+++ project/dao/main.py
@@ -1,6 +1,35 @@
-from project.dao.base import BaseDAO
-from project.models import Genre
+from typing import Optional, List
+from flask_sqlalchemy import BaseQuery
+from sqlalchemy import desc
+from werkzeug.exceptions import NotFound
+from project.dao.base import BaseDAO, T
+from project.models import Genre, Movie, Director, User
 
 
 class GenresDAO(BaseDAO[Genre]):
     __model__ = Genre
+
+
+class MoviesDAO(BaseDAO[Movie]):
+    __model__ = Movie
+
+    def get_all_order_by(self, filter: str, page: Optional[int] = None) -> List[T]:
+        stmt: BaseQuery = self._db_session.query(self.__model__)
+        if filter == 'new':
+            stmt = stmt.order_by(desc(self.__model__.year))
+        else:
+            stmt = stmt.order_by(self.__model__.year)
+        if page:
+            try:
+                return stmt.paginate(page, self._items_per_page).items
+            except NotFound:
+                return []
+        return stmt.all()
+
+
+class DirectorsDAO(BaseDAO[Director]):
+    __model__ = Director
+
+
+class UsersDAO(BaseDAO[User]):
+    __model__ = User
--- project/setup/api/models.py
+++ project/setup/api/models.py
@@ -6,3 +6,30 @@ genre: Model = api.model('Жанр', {
     'id': fields.Integer(required=True, example=1),
     'name': fields.String(required=True, max_length=100, example='Комедия'),
 })
+
+
+director: Model = api.model('Режиссеры', {
+    'id': fields.Integer(required=True, example=1),
+    'name': fields.String(required=True, max_length=100),
+})
+
+movie: Model = api.model('Фильмы', {
+    'id': fields.Integer(required=True, example=1),
+    'title': fields.String(required=True, max_length=100),
+    'description': fields.String(required=True, max_length=100),
+    'trailer': fields.String(required=True, max_length=100),
+    'year': fields.String(required=True, max_length=100),
+    'rating': fields.Float(required=True, max_length=100),
+    'genre_id': fields.Nested(genre),
+    'director_id': fields.Nested(director)
+})
+
+user: Model = api.model('Пользователи', {
+    'id': fields.Integer(required=True, example=1),
+    'email': fields.String(required=True, max_length=100),
+    'password': fields.String(required=True, max_length=280),
+    'name': fields.String(required=True, max_length=100),
+    'surname': fields.String(required=True, max_length=100),
+    'favorite_genre': fields.Nested(genre)
+
+})
